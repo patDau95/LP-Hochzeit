d9719c41b41ccc3ed3a065e073e7d4e5edce4466
Patrick Daucik <st149201@stud.uni-stuttgart.de>
2025-12-09 15:26:27 +0100

    modified:   index.html 	modified:   submit.js

--- submit.js
+++ submit.js
@@ -2,6 +2,11 @@ async function submitForm() {
   const params = new URLSearchParams(window.location.search);
   const id = params.get("id");
 
+  if (!id) {
+    alert("Kein Einladungslink gefunden!");
+    return;
+  }
+
   const body = {
     id,
     attending: document.getElementById("attending").value,
@@ -9,17 +14,21 @@ async function submitForm() {
     comment: document.getElementById("comment").value
   };
 
-  const response = await fetch(GAS_URL, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(body)
-  });
+  try {
+    const response = await fetch(GAS_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body)
+    });
 
-  const data = await response.json();
+    const data = await response.json();
 
-  if (data.success) {
-    alert("Danke! Deine Antwort wurde gespeichert.");
-  } else {
-    alert("Fehler beim Speichern.");
+    if (data.success) {
+      alert("Danke! Deine Antwort wurde gespeichert.");
+    } else {
+      alert("Fehler beim Speichern: " + (data.error || "Unbekannt"));
+    }
+  } catch (err) {
+    alert("Fehler beim Speichern: " + err.message);
   }
 }
